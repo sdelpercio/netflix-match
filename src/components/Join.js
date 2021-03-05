d78c2f57c7a0d430c807e1bb3e6c42598d94ef73
Shawn DelPercio <sdelperc@gmail.com>
2021-03-05 13:23:10 -0500

    added styles to join component

--- src/components/Join.js
+++ src/components/Join.js
@@ -6,26 +6,31 @@ const Join = () => {
   const [room, setroom] = useState("");
 
   return (
-    <div>
-      <h1>Join a room</h1>
-      <input
-        type="text"
-        placeholder="your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="room name"
-        value={room}
-        onChange={(e) => setroom(e.target.value)}
-      />
-      <Link
-        onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-        to={`/match/${name}/${room}/genres`}
-      >
-        <button type="submit">Join</button>
-      </Link>
+    <div className="m-auto mt-20 w-80 md:w-96">
+      <div className="flex flex-col items-center justify-evenly w-full h-60 shadow-xl rounded-2xl">
+        <h1 className="font-bold text-2xl">Join a room</h1>
+        <input
+          type="text"
+          placeholder="your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="shadow-inner h-10 w-3/4 text-lg pl-2"
+        />
+        <input
+          type="text"
+          placeholder="room name"
+          value={room}
+          onChange={(e) => setroom(e.target.value)}
+          className="shadow-inner h-10 w-3/4 text-lg pl-2"
+        />
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/match/${name}/${room}/genres`}
+          className="flex justify-center align-center shadow-md rounded-lg text-2xl font-extrabold w-1/2 transition duration-300 ease-in-out bg-white hover:bg-red-600 hover:text-white transform hover:-translate-y-1 hover:scale-110"
+        >
+          <button type="submit">Join</button>
+        </Link>
+      </div>
     </div>
   );
 };
