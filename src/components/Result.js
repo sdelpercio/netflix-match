c5137cb402c277b08610534a998290a4f672785a
Shawn DelPercio <sdelperc@gmail.com>
2021-04-08 15:56:44 -0400

    created api GET req for matched movies in results.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Result = ({ userMovies }) => {
+const Result = ({ userMovies, rapidApiKey }) => {
+  useEffect(() => {
+    if (userMovies.length !== 0) {
+      const products = [];
+      let requests = userMovies.map((id) => {
+        return new Promise((resolve, reject) => {
+          new Request(
+            `https://api.themoviedb.org/3/movie/${id}?api_key=${rapidApiKey}&language=en-US`,
+            { method: "GET" }
+          );
+        });
+      });
+      Promise.all(requests)
+        .then((body) => {
+          body.forEach((res) => {
+            if (res) {
+              products.push(JSON.parse(res));
+            }
+          });
+          console.log("results from promise.all", products);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [rapidApiKey, userMovies]);
+
   if (userMovies.length === 0) {
     return (
       <div className="m-auto mt-20 w-80 md:w-1/2">
